ab149ed11515e309ba1b8e28b2712f4e12702e42
Liu Ziyi <liuziyi@Lius-MacBook-Pro.local>
2018-04-01 16:54:06 +0800

    Change posts to posts auth for navbar and index.html

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => (
 	<div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
 		<h5 className="my-0 mr-md-auto font-weight-normal">
-			<Link className="p-2 text-dark" to="/">POSTS</Link>
+			<Link className="p-2 text-dark" to="/">POSTS REDUX</Link>
 		</h5>
 		<nav className="my-2 my-md-0 mr-md-3">
 			<Link className="p-2 text-dark" to="/about">About</Link>
